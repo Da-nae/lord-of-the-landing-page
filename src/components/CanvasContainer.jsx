52d4b48854b73245597a26ee85d3e244b275d356
Da-nae <grosjean.danae@gmail.com>
2023-03-09 14:17:29 +0100

    workshop to learn three fiber

---
+++ src/components/CanvasContainer.jsx
@@ -0,0 +1,34 @@
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import Model from "./Model.jsx";
+import { Suspense } from "react";
+import { DepthOfField, EffectComposer } from "@react-three/postprocessing";
+
+export default function CanvasContainer({ count = 80, depth = 65, z }) {
+
+    return (
+        <>
+        <div className={"title"}>
+            <h1>
+                Concerning Hobbits...
+            </h1>
+        </div>
+        <div className={"quote"}>
+            <p>"There and back again, A Hobbit’s tale, by Bilbo Baggins". Now, where to begin? </p>
+        </div>
+        <Canvas className={"canvas"} camera={{near: 0.01, far: 100, fov: 24}} dpr={1}>
+            <Suspense fallback={null}>
+                <color attach={"background"} args={["#629677"]} />
+                <ambientLight intensity={0.5} />
+                {Array.from({length: count}, ( _ ,  i) => (
+                    <Model key={i} z={ -i / count  * depth} />
+                ))}
+                <Environment preset={"city"} blur={1} />
+                    <EffectComposer>
+                        <DepthOfField target={[0, 0, depth / 2]} focalLength={0.4} bokehScale={12} height={800} />
+                    </EffectComposer>
+            </Suspense>
+        </Canvas>
+        </>
+    );
+}
